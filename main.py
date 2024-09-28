1b5f318abd5483473a605fb90e040d227eb4c083
DanielSouto <dsoutopazo3@gmail.com>
2024-09-28 22:24:08 +0200

    Corrección do main.py

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ print(f"El area de un cuadrado de lado {lado} es: {cuadrado.get_area(lado)} y el
 
 base = 4
 altura = 2
-print(cuadrado.get_identificador())
+print(triangulo.get_identificador())
 print(f"El area de un triángulo de base {base} y altura {altura} es: {triangulo.get_area(base, altura)} y el perímetro es {triangulo.get_perimetro(base, base, base)}")
 
 base = 2
 altura = 4
+print(rectangulo.get_identificador())
 print(f"El area de un rectangulo de base {base} y altura {altura} es: {rectangulo.get_area(base, altura)} y el perímetro es {rectangulo.get_perimetro(base, altura)}")

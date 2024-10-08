65bf1805059abf4b8dd6e763e28d0ace9a370113
moucho-chosco <dsoutopazo3@gmail.com>
2024-10-08 16:53:23 +0200

    Agregadas funcionalidades para la circunferencia

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from lib import cuadrado, triangulo, rectangulo
+from lib import cuadrado, triangulo, rectangulo, circunferencia
 print("Proyecto Figuras")
 print(cuadrado.get_identificador())
 lado=4
@@ -13,3 +13,6 @@ base = 2
 altura = 4
 print(rectangulo.get_identificador())
 print(f"El area de un rectangulo de base {base} y altura {altura} es: {rectangulo.get_area(base, altura)} y el perímetro es {rectangulo.get_perimetro(base, altura)}")
+
+radio = 2
+print(f"El área de una circunferencia de radio {radio} es: {circunferencia.get_area_circulo(radio)}")
